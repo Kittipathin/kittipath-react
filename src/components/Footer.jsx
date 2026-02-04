7448e11a65c593789c158e6d9b7145dff8d05fb5
33@gmail.com <you@example.com>
2026-02-04 20:03:56 +0700

    nwechange2003

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ export const Footer = () => {
             backgroundColor: '#34495e', // สีโทนเดียวกับ Header แต่อ่อนลงนิดนึง
             color: 'white',
             textAlign: 'center',
-            padding: '25px 0',
-            fontSize: '0.9rem',
+            padding: '20px 0',
+            fontSize: '1rem',
             // ถ้าเนื้อหาน้อย Footer จะลอยขึ้นมา ใช้ margin-top ช่วยได้ระดับนึงใน App.js
         }}>
             &copy; Department of Computer Education, KMUTNB
